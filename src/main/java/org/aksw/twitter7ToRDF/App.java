ae94ce4591ebf4d02bc99a626562cf1ec71f2656
felixlinker <felixlinker@outlook.com>
2016-05-26 12:01:26 +0200

    Created maven project

---
+++ src/main/java/org/aksw/twitter7ToRDF/App.java
@@ -0,0 +1,13 @@
+package org.aksw.twitter7ToRDF;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
