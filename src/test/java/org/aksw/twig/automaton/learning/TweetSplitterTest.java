7cd7015a2966c818ce8fd0054df30fa65a11969f
felixlinker <felixlinker@outlook.com>
2016-06-09 18:12:53 +0200

    Added TweetSplitter

---
+++ src/test/java/org/aksw/twig/automaton/learning/TweetSplitterTest.java
@@ -0,0 +1,32 @@
+package org.aksw.twig.automaton.learning;
+
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class TweetSplitterTest {
+
+    private static final Logger LOGGER = LogManager.getLogger(TweetSplitter.class);
+
+    @Test
+    public void splitTweet() {
+        TweetSplitter splitter = new TweetSplitter("@Audition_Portal PAAAUULLLL! I miss youuu! Lol I thought I sent you an email, but it was placed in my Drafts so I'll resend that to you! =]");
+        Set<Pair<String, String>> tweet = splitter.getSplit().collect(Collectors.toSet());
+        tweet.forEach(pair -> LOGGER.info("{} {}", pair.getLeft(), pair.getRight()));
+        // Check randoms
+        Assert.assertTrue(tweet.contains(new ImmutablePair<>("miss", "youuu")));
+        Assert.assertTrue(tweet.contains(new ImmutablePair<>("Drafts", "so")));
+        // Check sentence start
+        Assert.assertTrue(tweet.contains(new ImmutablePair<>("", "I")));
+        // Check sentence end
+        Assert.assertTrue(tweet.contains(new ImmutablePair<>("you", "")));
+        // Check punctuation
+        Assert.assertTrue(tweet.contains(new ImmutablePair<>("email", "but")));
+    }
+}
