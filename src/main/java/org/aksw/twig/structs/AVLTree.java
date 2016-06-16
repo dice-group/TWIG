d85ee45203b585372be2429ccbb289068321b355
felixlinker <felixlinker@outlook.com>
2016-06-16 17:52:23 +0200

    AVL Tree improved

--- src/main/java/org/aksw/twig/structs/AVLTree.java
+++ src/main/java/org/aksw/twig/structs/AVLTree.java
@@ -1,8 +1,5 @@
 package org.aksw.twig.structs;
 
-import org.apache.commons.lang3.tuple.ImmutableTriple;
-import org.apache.commons.lang3.tuple.Triple;
-
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -11,46 +8,58 @@ public class AVLTree<T extends Comparable<T>> implements Collection<T> {
 
     private AVLNode root;
 
-    private int size;
-
-    private int height;
-
-    private Class<T> clazz;
+    private int size = 0;
 
     @Override
     public int size() {
-        return this.size;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return this.size() == 0;
+        return size() == 0;
+    }
+
+    /**
+     * Behaves like {@link Collection#contains(Object)} but is guaranteed to run in {@code O(log n)} with {@code n} being amount of values in this tree.
+     * The default implementation of {@link Collection#contains(Object)} will run in {@code O(n)}.
+     * @param value Value to check presence of.
+     * @return {@code true} if and only if value is present in the tree.
+     */
+    public boolean contains(final T value) {
+        if (root == null || value == null) {
+            return false;
+        }
+
+        AVLNode node = root.traverse(value, true);
+        return node.val.equals(value);
     }
 
     @Override
-    public boolean contains(Object o) {
-        if (this.root == null) {
+    public boolean contains(final Object o) {
+        if (o == null) {
             return false;
         }
 
-        if (!this.clazz.isInstance(o)) {
-            return false;
+        Iterator<T> iterator = iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().equals(o)) {
+                return true;
+            }
         }
 
-        T val = this.clazz.cast(o);
-        Triple<AVLNode, AVLNode, Integer> triple = this.root.traverse(val, true);
-        return triple.getMiddle().val.equals(val);
+        return false;
     }
 
     @Override
     public Iterator<T> iterator() {
-        return null; // TODO
+        return new AVLIterator();
     }
 
     @Override
     public Object[] toArray() {
-        Iterator<T> iterator = this.iterator();
-        Object[] array = new Object[this.size];
+        Iterator<T> iterator = iterator();
+        Object[] array = new Object[size()];
         int i = 0;
         while (iterator.hasNext()) {
             array[i++] = iterator.next();
@@ -60,59 +69,49 @@ public class AVLTree<T extends Comparable<T>> implements Collection<T> {
     }
 
     @Override
-    public <V> V[] toArray(V[] a) {
+    public <V> V[] toArray(final V[] a) {
         if (a.length < size) {
-            return (V[]) this.toArray();
+            return (V[]) toArray();
         }
 
         LinkedList<Integer> l = new LinkedList<>();
 
-        Iterator<T> iterator = this.iterator();
+        Iterator<T> iterator = iterator();
         Object[] result = a;
         int i = 0;
         while (iterator.hasNext()) {
             result[i++] = iterator.next();
         }
 
-        if (this.size < a.length) {
-            a[this.size] = null;
+        if (size() < a.length) {
+            a[size()] = null;
         }
 
         return a;
     }
 
     @Override
-    public boolean add(T t) {
-        // TODO grant AVL property
-        if (this.root == null) {
-            this.root = new AVLNode(t);
-            this.size++;
+    public boolean add(final T t) {
+        if (root == null) {
+            root = new AVLNode(t, null);
+            size++;
             return true;
         }
 
-        Triple<AVLNode, AVLNode, Integer> triple = this.root.traverse(t, false);
-        triple.getMiddle().entail(t);
-        this.size++;
+        // TODO grant AVL property
+        root.traverse(t, false).entail(t);
+        size++;
         return true;
     }
 
-    @Override
-    public boolean remove(Object o) {
-        if (this.root == null) {
+    public boolean remove(final T value) {
+        if (root == null || value == null) {
             return false;
         }
 
-        if (!this.clazz.isInstance(o)) {
-            return false;
-        }
-
-        T val = this.clazz.cast(o);
-        Triple<AVLNode, AVLNode, Integer> triple = this.root.traverse(val, true);
-        if (triple.getMiddle().val.equals(val)) {
-            // TODO grant AVL property
-            triple.getLeft().gtr = triple.getMiddle().gtr;
-            triple.getMiddle().gtr.leq = triple.getMiddle().leq;
-            this.size--;
+        AVLNode toRemove = root.traverse(value, true);
+        if (toRemove.val.equals(value)) {
+            // TODO
             return true;
         }
 
@@ -120,38 +119,56 @@ public class AVLTree<T extends Comparable<T>> implements Collection<T> {
     }
 
     @Override
-    public boolean containsAll(Collection<?> c) {
-        return c.stream().allMatch(element -> this.contains(element));
+    public boolean remove(final Object o) {
+        if (o == null) {
+            return false;
+        }
+
+        Iterator<T> iterator = iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().equals(iterator)) {
+                // TODO
+                return true;
+            }
+        }
+
+
+        return false;
     }
 
     @Override
-    public boolean addAll(Collection<? extends T> c) {
+    public boolean containsAll(final Collection<?> c) {
+        return c.stream().allMatch(element -> contains(element));
+    }
+
+    @Override
+    public boolean addAll(final Collection<? extends T> c) {
         boolean changed = false;
         for (T element: c) {
-            changed |= this.add(element);
+            changed |= add(element);
         }
 
         return changed;
     }
 
     @Override
-    public boolean removeAll(Collection<?> c) {
+    public boolean removeAll(final Collection<?> c) {
         boolean changed = false;
         for (Object element: c) {
-            changed |= this.remove(element);
+            changed |= remove(element);
         }
         return changed;
     }
 
     @Override
-    public boolean retainAll(Collection<?> c) {
+    public boolean retainAll(final Collection<?> c) {
         return false; // TODO
     }
 
     @Override
     public void clear() {
-        this.root = null;
-        this.size = 0;
+        root = null;
+        size = 0;
     }
 
     private class AVLNode {
@@ -159,47 +176,65 @@ public class AVLTree<T extends Comparable<T>> implements Collection<T> {
 
         private T val;
 
+        private AVLNode parent;
+
         private AVLNode leq;
 
         private AVLNode gtr;
 
-        public AVLNode(T val) {
+        AVLNode(final T val, final AVLNode parent) {
             this.val = val;
+            this.parent = parent;
         }
 
-        public boolean hasNext(T val) {
+        boolean hasNext(final T val) {
             return val.compareTo(val) <= 0 ? leq != null : gtr != null;
         }
 
-        public AVLNode next(T val) {
+        AVLNode next(final T val) {
             return val.compareTo(val) <= 0 ? leq : gtr;
         }
 
-        public Triple<AVLNode, AVLNode, Integer> traverse(T val, boolean lookup) {
-            int level = 0;
-
-            AVLNode predecessor = null;
+        AVLNode traverse(final T val, final boolean lookup) {
             AVLNode last = this;
             while (last.hasNext(val) || (lookup && last.val.equals(val))) {
-                level++;
-                predecessor = last;
                 last = last.next(val);
             }
 
-            return new ImmutableTriple<>(predecessor, last, level);
+            return last;
         }
 
-        public AVLNode entail(T value) {
+        AVLNode entail(final T value) {
             AVLNode prev;
-            if (this.val.compareTo(value) <= 0) {
-                prev = this.leq;
-                this.leq = new AVLNode(value);
+            if (val.compareTo(value) <= 0) {
+                prev = leq;
+                leq = new AVLNode(value, this);
             } else {
-                prev = this.gtr;
-                this.gtr = new AVLNode(value);
+                prev = gtr;
+                gtr = new AVLNode(value, this);
             }
 
+            if (prev != null) {
+                prev.parent = null;
+            }
             return prev;
+        }
+
+        int depth() {
+            return leq.depth() > gtr.depth() ? leq.depth() : gtr.depth();
+        }
+    }
+
+    private class AVLIterator implements Iterator<T> {
+
+        @Override
+        public boolean hasNext() {
+            return false; // TODO
+        }
+
+        @Override
+        public T next() {
+            return null; // TODO
         }
     }
 }
