f63c3bf46afd3b7da9b648dc3b410901b025f105
felixlinker <felixlinker.github@outlook.com>
2016-07-21 15:20:00 +0200

    Added build process

---
+++ src/main/java/org/aksw/twig/Main.java
@@ -0,0 +1,24 @@
+package org.aksw.twig;
+
+import org.aksw.twig.parsing.Twitter7Parser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Arrays;
+
+public class Main {
+
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            LOGGER.error("No arguments given. To get an overview please use the argument --help.");
+            return;
+        }
+
+        switch (args[0]) {
+            case "--help": LOGGER.info("Use 'Twitter7Parser arg0 arg1 ...' to parse twitter data."); break;
+            case "Twitter7Parser": Twitter7Parser.main(Arrays.copyOfRange(args, 1, args.length)); break;
+        }
+    }
+}
