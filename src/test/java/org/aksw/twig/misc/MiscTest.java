22ea5a189bfb936fff35f9fa74cf4cc13eb83c4e
felixlinker <linkerfelix@gmail.com>
2016-07-25 12:02:52 +0200

    Test added

---
+++ src/test/java/org/aksw/twig/misc/MiscTest.java
@@ -0,0 +1,12 @@
+package org.aksw.twig.misc;
+
+import org.apache.jena.rdf.model.ModelFactory;
+import org.junit.Test;
+
+public class MiscTest {
+
+    @Test
+    public void test() {
+        ModelFactory.createDefaultModel();
+    }
+}
