b9795c7ddd4712c525c90ef79a9dd5f072cca90a
felixlinker <linkerfelix@gmail.com>
2016-07-25 12:25:34 +0200

    Updated test

--- src/test/java/org/aksw/twig/misc/MiscTest.java
+++ src/test/java/org/aksw/twig/misc/MiscTest.java
@@ -2,18 +2,36 @@ package org.aksw.twig.misc;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class MiscTest {
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-    private static final Logger LOGGER = LogManager.getLogger(MiscTest.class);
+public class MiscTest implements Callable<Void> {
 
     public final Model model = ModelFactory.createDefaultModel();
 
+    @Override
+    public Void call() throws Exception {
+        new MiscTest();
+        return null;
+    }
+
     @Test
     public void test() {
-        LOGGER.info("Still here");
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        List<Callable<Void>> callables = new LinkedList<>();
+        for (int i = 0; i < 100; i++) {
+            callables.add(new MiscTest());
+        }
+        try {
+            executorService.invokeAll(callables);
+        } catch (InterruptedException e) {
+            Assert.fail();
+        }
     }
 }
