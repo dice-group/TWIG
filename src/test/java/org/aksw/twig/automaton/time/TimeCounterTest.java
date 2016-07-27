032a5b17e41f704179bb9194e5d4694fc9a31f5b
felixlinker <linkerfelix@gmail.com>
2016-07-27 13:11:24 +0200

    Test more robust

--- src/test/java/org/aksw/twig/automaton/time/TimeCounterTest.java
+++ src/test/java/org/aksw/twig/automaton/time/TimeCounterTest.java
@@ -10,14 +10,14 @@ public class TimeCounterTest {
     @Test
     public void test() {
         TimeCounter counter = new TimeCounter();
-        LocalDateTime now = LocalDateTime.now().withMinute(0);
-        LocalDateTime nowPlusOneSecond = now.withSecond(now.getSecond() + 1);
-        LocalDateTime nowPlusOneMinute = now.withMinute(now.getMinute() + 1);
-        counter.addTweetTime(now, 2);
-        counter.addTweetTime(nowPlusOneSecond, 1);
-        counter.addTweetTime(nowPlusOneMinute, 3);
+        LocalDateTime testTime = LocalDateTime.of(1995, 2, 12, 12, 0, 0);
+        LocalDateTime testTimePlusOneSecond = testTime.withSecond(testTime.getSecond() + 1);
+        LocalDateTime testTimePlusOneMinute = testTime.withMinute(testTime.getMinute() + 1);
+        counter.addTweetTime(testTime, 2);
+        counter.addTweetTime(testTimePlusOneSecond, 1);
+        counter.addTweetTime(testTimePlusOneMinute, 3);
 
-        Assert.assertEquals(3, counter.getTweetTimesAt(now.getHour(), now.getMinute()));
-        Assert.assertEquals(3, counter.getTweetTimesAt(nowPlusOneMinute.getHour(), nowPlusOneMinute.getMinute()));
+        Assert.assertEquals(3, counter.getTweetTimesAt(testTime.getHour(), testTime.getMinute()));
+        Assert.assertEquals(3, counter.getTweetTimesAt(testTimePlusOneMinute.getHour(), testTimePlusOneMinute.getMinute()));
     }
 }
