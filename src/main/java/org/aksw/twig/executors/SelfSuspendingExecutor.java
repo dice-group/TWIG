b6e9d8a1a6a1773ae4d27f93988aa5f1741c8380
felixlinker <linkerfelix@gmail.com>
2016-08-11 12:10:37 +0200

    Doc

--- src/main/java/org/aksw/twig/executors/SelfSuspendingExecutor.java
+++ src/main/java/org/aksw/twig/executors/SelfSuspendingExecutor.java
@@ -10,6 +10,12 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * This class works like a {@link Executors#newFixedThreadPool(int)} with one difference. {@link Callable} objects get pulled from a {@link SuspendSupplier} which will then be executed.
+ * Results will be handed vice versa to the {@link SuspendSupplier}. The major advantage of this class is that it will only pull and execute {@link Callable} objects from its supplier as long as there are available threads.
+ * When there are no free threads available the executor will wait until there are. This is especially useful if queued tasks on normal executors consume a lot of RAM or task creation is much more faster than task processing.
+ * @param <T> Type of the results provided by the {@link Callable} objects.
+ */
 public class SelfSuspendingExecutor<T> {
 
     private static final Logger LOGGER = LogManager.getLogger(SelfSuspendingExecutor.class);
@@ -18,25 +24,44 @@ public class SelfSuspendingExecutor<T> {
 
     private final SuspendSupplier<T> suspendSupplier;
 
+    /**
+     * Executes by {@link #suspendSupplier} supplied callables.
+     */
     private final ExecutorService executorService;
 
+    /**
+     * Executes code querying the {@link #suspendSupplier}.
+     */
     private final ExecutorService callableSubmittingService;
 
     private final Queue<Runnable> finishedEventListeners = new LinkedList<>();
 
     private boolean finished = false;
 
+    /**
+     * Creates a new instance setting class variables.
+     * @param suspendSupplier Supplier for new callables.
+     */
     public SelfSuspendingExecutor(SuspendSupplier<T> suspendSupplier) {
         this.suspendSupplier = suspendSupplier;
         this.executorService = Executors.newFixedThreadPool(nThreads);
         this.callableSubmittingService = Executors.newSingleThreadExecutor();
     }
 
+    /**
+     * Creates a new instance setting class variables.
+     * @param suspendSupplier Supplier for new callables.
+     * @param nThreads Number of threads to work on callables.
+     */
     public SelfSuspendingExecutor(SuspendSupplier<T> suspendSupplier, int nThreads) {
         this(suspendSupplier);
         this.nThreads = nThreads;
     }
 
+    /**
+     * Adds finished listeners to the executor. Listeners will be executed once all callables have been executed. If every callable has already been executed listeners will be run immediately.
+     * @param listeners Listeners to execute.
+     */
     public void addFinishedEventListeners(Runnable... listeners) {
         synchronized (finishedEventListeners) {
             if (finished) {
@@ -49,6 +74,9 @@ public class SelfSuspendingExecutor<T> {
         }
     }
 
+    /**
+     * Starts callable processing. Method is non-blocking, i. e. termination of this method does not mean every callable has been executed.
+     */
     public void start() {
         callableSubmittingService.execute(() -> {
             for (int i = 0; i < nThreads; i++) {
@@ -57,6 +85,9 @@ public class SelfSuspendingExecutor<T> {
         });
     }
 
+    /**
+     * Executes the next callable supplied by {@link #suspendSupplier} or finishes execution.
+     */
     private void execNext() {
         LOGGER.info("Querying callable");
         Callable<T> callable = suspendSupplier.next();
@@ -69,6 +100,9 @@ public class SelfSuspendingExecutor<T> {
         executorService.execute(() -> runCallable(callable));
     }
 
+    /**
+     * Finishes the execution by shutting down services, etc.
+     */
     private void finish() {
         if (executorService.isShutdown()) {
             return;
@@ -94,12 +128,20 @@ public class SelfSuspendingExecutor<T> {
         }
     }
 
+    /**
+     * Returns whether every callable has been executed.
+     * @return True iff every callable has been executed.
+     */
     public boolean isFinished() {
         synchronized (finishedEventListeners) {
             return finished;
         }
     }
 
+    /**
+     * Executes a callable.
+     * @param callable Callable to execute.
+     */
     private void runCallable(Callable<T> callable) {
         try {
             LOGGER.info("Executing callable");
