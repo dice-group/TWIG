29544b6b03806e4a1bf0aa538cd385ab0ef5d40c
felixlinker <felixlinker.github@outlook.com>
2016-08-12 11:02:08 +0200

    Main arg added

--- src/main/java/org/aksw/twig/Main.java
+++ src/main/java/org/aksw/twig/Main.java
@@ -5,6 +5,7 @@ import org.aksw.twig.automaton.messageCount.MessageCounterHandler;
 import org.aksw.twig.automaton.time.TimeCounter;
 import org.aksw.twig.automaton.time.TimeCounterHandler;
 import org.aksw.twig.automaton.tweets.WordMatrix;
+import org.aksw.twig.automaton.tweets.WordMatrixHandler;
 import org.aksw.twig.parsing.Twitter7Parser;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,6 +28,7 @@ public class Main {
             case "MessageCounter": MessageCounter.main(Arrays.copyOfRange(args, 1, args.length)); break;
             case "MessageCounterHandler": MessageCounterHandler.main(Arrays.copyOfRange(args, 1, args.length)); break;
             case "WordMatrix": WordMatrix.main(Arrays.copyOfRange(args, 1, args.length)); break;
+            case "WordMatrixHandler": WordMatrixHandler.main(Arrays.copyOfRange(args, 1, args.length)); break;
             case "TimeCounter": TimeCounter.main(Arrays.copyOfRange(args, 1, args.length)); break;
             case "TimeCounterHandler": TimeCounterHandler.main(Arrays.copyOfRange(args, 1, args.length)); break;
             default: LOGGER.info("No argument recognized. To get an overview please use the argument --help.");
