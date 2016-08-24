9a1317fca8fe412a9f49fc42c00477fea8749af6
felixlinker <linkerfelix@gmail.com>
2016-08-24 12:34:30 +0200

    package-info.java files added

---
+++ src/main/java/org/aksw/twig/automaton/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Classes of this package use data analysis results of classes in sub-package {@code data} to generate tweets.
+ * Executable class is {@link org.aksw.twig.automaton.Automaton}.
+ */
+package org.aksw.twig.automaton;
---
+++ src/main/java/org/aksw/twig/parsing/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes of this package process data in Twitter7 format resulting in a TWIG model.
+ * The executable class is {@link org.aksw.twig.parsing.Twitter7Parser}.
+ * Other classes are for package-internal usage only.
+ */
+package org.aksw.twig.parsing;
