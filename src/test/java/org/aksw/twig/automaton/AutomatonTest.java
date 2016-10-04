67effd5f287d855e90ab8d027ce885213e910dfa
felixlinker <linkerfelix@gmail.com>
2016-10-04 16:00:53 +0200

    Added manual test temporarily

--- src/test/java/org/aksw/twig/automaton/AutomatonTest.java
+++ src/test/java/org/aksw/twig/automaton/AutomatonTest.java
@@ -16,10 +16,49 @@ import java.io.ObjectInputStream;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AutomatonTest {
 
     private static final Logger LOGGER = LogManager.getLogger(AutomatonTest.class);
+
+//    @Test
+    public void tmpTest() {
+
+        WordMatrix wordMatrix_0;
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("C:/users/felix/desktop/TWIG_matrix/word_matrix_0.obj"))) {
+            wordMatrix_0 = (WordMatrix) inputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            LOGGER.error(e.getMessage(), e);
+            Assert.fail();
+            return;
+        }
+
+        WordMatrix wordMatrix_1;
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("C:/users/felix/desktop/TWIG_matrix/word_matrix_1.obj"))) {
+            wordMatrix_1 = (WordMatrix) inputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            LOGGER.error(e.getMessage(), e);
+            Assert.fail();
+            return;
+        }
+
+        Set<String> predecessors_0 = wordMatrix_0.getPredecessors();
+        Set<String> predecessors_1 = wordMatrix_1.getPredecessors();
+        int size_0 = predecessors_0.size();
+        int size_1 = predecessors_1.size();
+
+        Set<String> all = Stream.concat(predecessors_0.stream(), predecessors_1.stream()).collect(Collectors.toSet());
+        int size = all.size();
+
+        double growRate = (((double) size / (double) size_0) + ((double) size / (double) size_1)) / 2;
+
+        LOGGER.info(size);
+        LOGGER.info(growRate);
+
+    }
 
 //    @Test
     public void AutomatonTest() {
