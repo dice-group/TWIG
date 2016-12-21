41c56a45518c79a0d2bf792842985c8951e44136
renespeck <speckrene@gmail.com>
2016-12-21 02:26:52 +0100

    Change parameter.

--- sample.sh
+++ sample.sh
@@ -18,4 +18,4 @@ java -jar target/twig-parent-0.0.1-SNAPSHOT.jar WordMatrixHandler --out=sample/a
 # 2010-01-01 -> start date of simulation, i. e. it will be simulated from 2010-01-01 to 2010-02-03
 # 1 -> seed value
 # sample/output -> Folder for results
-java -jar target/twig-parent-0.0.1-SNAPSHOT.jar Automaton sample/analysis/word_matrix_0.obj sample/analysis/message_count_0.obj sample/analysis/time_count_0.obj 2 2 2010-01-01 1 sample/output
+java -jar target/twig-parent-0.0.1-SNAPSHOT.jar Automaton sample/analysis/word_matrix_0.obj sample/analysis/message_count_0.obj sample/analysis/time_count_0.obj 10 30 2009-09-29 1 sample/output
