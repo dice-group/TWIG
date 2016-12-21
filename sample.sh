9c7d8bd6ca3177826109eb0aa0b304ccf0e43424
renespeck <speckrene@gmail.com>
2016-12-21 02:25:08 +0100

    Update sample.

--- sample.sh
+++ sample.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+rm -r sample/{analysis,data,output}
+
+mkdir -p sample/{analysis,data,output}
 
 #parse data
 java -jar target/twig-parent-0.0.1-SNAPSHOT.jar Twitter7Parser --out=sample/data sample/sample.txt.gz
