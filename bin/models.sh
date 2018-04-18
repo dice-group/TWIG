e51aae793616a3ec0902b72b7a1573cfd0001dbe
Rene Speck <speckrene@gmail.com>
2018-04-18 18:27:11 +0200

    Version 0.0.4.

--- bin/models.sh
+++ bin/models.sh
@@ -3,6 +3,6 @@
 cd ..
 
 # create models
-java -jar target/twig-parent-0.0.3-SNAPSHOT.jar MessageCounterHandler --out=sample/analysis --in=sample/data
-java -jar target/twig-parent-0.0.3-SNAPSHOT.jar TimeCounterHandler --out=sample/analysis --in=sample/data
-java -jar target/twig-parent-0.0.3-SNAPSHOT.jar WordMatrixHandler --out=sample/analysis --in=sample/data
+java -jar target/twig-parent-0.0.4-SNAPSHOT.jar MessageCounterHandler --out=sample/analysis --in=sample/data
+java -jar target/twig-parent-0.0.4-SNAPSHOT.jar TimeCounterHandler --out=sample/analysis --in=sample/data
+java -jar target/twig-parent-0.0.4-SNAPSHOT.jar WordMatrixHandler --out=sample/analysis --in=sample/data
