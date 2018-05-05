412bab73f4cbfff78dabaf2087382ed6166c27d9
René Speck <speckrene@gmail.com>
2018-05-05 20:44:03 +0200

    Update mimic.sh

--- mimic.sh
+++ mimic.sh
@@ -2,6 +2,6 @@
 
 export MAVEN_OPTS="-Xmx50G"
 
-ARGS="Automaton data/word_matrix_1.obj data/message_count_0.obj data/time_count_0.obj 10 14 2009-09-29 1 mimic"
+ARGS="Automaton data/word_matrix_0.obj data/message_count_0.obj data/time_count_0.obj 10 14 2009-09-29 1 mimic"
 
 nohup mvn exec:java -Dexec.mainClass="org.aksw.twig.Main" -Dexec.args="$ARGS" > mimic.log &
