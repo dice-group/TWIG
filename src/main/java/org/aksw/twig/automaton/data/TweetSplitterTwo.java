b51ded6328080195c884095574d860de242819d0
Rene Speck <speckrene@gmail.com>
2020-04-07 18:45:23 +0200

    Adds log.

--- src/main/java/org/aksw/twig/automaton/data/TweetSplitterTwo.java
+++ src/main/java/org/aksw/twig/automaton/data/TweetSplitterTwo.java
@@ -7,12 +7,17 @@ import java.util.stream.Stream;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import cmu.arktweetnlp.Twokenize;
 
 /**
  */
 class TweetSplitterTwo implements Iterable<Pair<String, String>> {
+
+  private static final Logger LOG = LogManager.getLogger(TweetSplitterTwo.class);
+
   /**
    * Creates a new instance and splits given tweet.
    *
@@ -45,16 +50,20 @@ class TweetSplitterTwo implements Iterable<Pair<String, String>> {
    * @param tweet Tweet to split words in.
    */
   private void splitTweet(final String tweet) {
-    String lastWord = "";
-    for (final String word : Twokenize.tokenizeRawTweetText(tweet)) {
-      if (word.equals("")) {
-        continue;
+    try {
+      String lastWord = "";
+      for (final String word : Twokenize.tokenizeRawTweetText(tweet)) {
+        if (word.equals("")) {
+          continue;
+        }
+        split.add(new ImmutablePair<>(lastWord, word));
+        lastWord = word;
       }
-      split.add(new ImmutablePair<>(lastWord, word));
-      lastWord = word;
-    }
-    if (!lastWord.equals("")) {
-      split.add(new ImmutablePair<>(lastWord, ""));
+      if (!lastWord.equals("")) {
+        split.add(new ImmutablePair<>(lastWord, ""));
+      }
+    } catch (final Exception e) {
+      LOG.error(e.getLocalizedMessage(), e);
     }
   }
 }
