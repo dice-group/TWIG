f64ef095d683c20be46f87902ba6430833b9a2b9
Rene Speck <speckrene@gmail.com>
2020-04-19 20:39:47 +0200

    Update, reduces memory used while truncate a wordmatix.

--- src/main/java/org/aksw/twig/application/LoadModels.java
+++ src/main/java/org/aksw/twig/application/LoadModels.java
@@ -9,11 +9,18 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.stream.Collectors;
 
 import org.aksw.twig.Const;
 import org.aksw.twig.automaton.data.WordMatrix;
 import org.aksw.twig.files.FileHandler;
+import org.apache.commons.lang3.tuple.MutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,47 +34,99 @@ public class LoadModels {
 
   private static final Logger LOG = LogManager.getLogger(LoadModels.class);
 
-  private final long maxMemory = Const.DEFAULT_MAX_MEMORY < 1 ? //
-      RuntimeUtil.getMaxMemory() : Const.DEFAULT_MAX_MEMORY;
-
-  private final long usedMemory = Double.valueOf(maxMemory * Const.MEMORY_USE).longValue();
-
   private final double defaultTruncateTo = Const.DEFAULT_TRUNCATE_TO;
   private double truncateTo = defaultTruncateTo;
 
+  private final int maxMemory = Long.valueOf(Const.DEFAULT_MAX_MEMORY).intValue();
+
   public WordMatrix wordMatrixMerged = null;
 
+  public LoadModels() {
+
+  }
+
+  /**
+   *
+   * @param files
+   */
   public LoadModels(final Set<File> files) {
 
-    LOG.info(//
-        "max memory: {}, half: {}, defaultTruncateTo: {}, truncateTo: {} ", //
-        maxMemory, usedMemory, defaultTruncateTo, truncateTo//
-    );
-
     for (final File file : files) {
-      final WordMatrix wordMatrix = loadWordMatrix(file);
-      truncate(wordMatrix);
+      WordMatrix wordMatrix = loadWordMatrix(file);
+      LOG.info("wordMatrix size: {}", wordMatrix.matrix.keySet().size());
+      wordMatrix = truncate(wordMatrix);
       if (wordMatrixMerged == null) {
         wordMatrixMerged = wordMatrix;
       } else {
         wordMatrixMerged.merge(wordMatrix);
-        truncate(wordMatrixMerged);
+        wordMatrixMerged = truncate(wordMatrixMerged);
       }
     } // end for
-
   }
 
-  protected void truncate(final WordMatrix wordMatrix) {
-    while (RuntimeUtil.getUsedMemory() > usedMemory) {
-      wordMatrix.truncateTo(truncateTo);
-      truncateTo = truncateTo + truncateTo;
-      LOG.info("truncateTo: {}, used memory/max: {}/{}", //
-          truncateTo, RuntimeUtil.getUsedMemoryInMiB(), RuntimeUtil.bytesToMiB(usedMemory));
+  protected WordMatrix truncate(final WordMatrix wordMatrix) {
+    while (wordMatrix.matrix.keySet().size() > maxMemory && truncateTo <= 1) {
+      wordMatrix.matrix = truncateTo(truncateTo, wordMatrix.matrix);
+      truncateTo = truncateTo + defaultTruncateTo;
+      LOG.info("runcated to: {}", truncateTo);
     }
     truncateTo = defaultTruncateTo;
+    return wordMatrix;
   }
 
-  protected WordMatrix loadWordMatrix(final File file) {
+  public Map<String, MutablePair<Long, Map<String, Long>>> truncateTo(final double lowerBoundChance,
+      final Map<String, MutablePair<Long, Map<String, Long>>> matrix) {
+
+    // elements for median calculation
+    final Queue<Long> predecessors = new ConcurrentLinkedQueue<>();
+    matrix.entrySet().parallelStream().forEach(entry -> {
+      predecessors.add(entry.getValue().getLeft().longValue());
+    });
+
+    // median calculation
+    final long median =
+        median(lowerBoundChance, predecessors.stream().collect(Collectors.toList()));
+
+    // removes predecessor elements below median
+    matrix.entrySet().removeIf(s -> s.getValue().getLeft() < median);
+
+    matrix.entrySet().stream().forEach(entry -> {
+      final String predecessor = entry.getKey();
+      final MutablePair<Long, Map<String, Long>> v = entry.getValue();
+
+      final long successorsMedian = median(lowerBoundChance, v.getRight().values());
+
+      final Map<String, Long> successors = v.getRight();
+      successors.entrySet().removeIf(s -> s.getValue().doubleValue() < successorsMedian);
+
+      if (!successors.isEmpty()) {
+        final long newCount =
+            successors.entrySet().stream().map(Map.Entry::getValue).reduce(0L, Long::sum);
+        matrix.put(predecessor, new MutablePair<>(newCount, successors));
+      }
+    });
+    return matrix;
+  }
+
+  /**
+   *
+   * @param border
+   * @param unsorted
+   * @return list element
+   */
+  protected <T> T median(final double border, final Collection<T> unsorted) {
+    final List<T> sorted = unsorted.stream().sorted().collect(Collectors.toList());
+    final int index = Long.valueOf(Math.round(sorted.size() * border)).intValue();
+    // LOG.info("size: {}, index: {}", unsorted.size(), index);
+    return sorted.get(index >= sorted.size() ? sorted.size() - 1 : index);
+  }
+
+  /**
+   *
+   * @param file
+   * @return
+   */
+  public WordMatrix loadWordMatrix(final File file) {
     ObjectInputStream stream = null;
     WordMatrix wordMatrix = null;
     try {
@@ -86,10 +145,19 @@ public class LoadModels {
     return wordMatrix;
   }
 
-  protected WordMatrix loadWordMatrix(final String file) {
+  /**
+   *
+   * @param file
+   * @return
+   */
+  public WordMatrix loadWordMatrix(final String file) {
     return loadWordMatrix(Paths.get(file).toFile());
   }
 
+  /**
+   *
+   * @param args
+   */
   public static void main(final String[] args) {
 
     final Pair<File, Set<File>> parsedArgs = FileHandler.readArgs(args);
